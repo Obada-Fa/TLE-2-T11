fd031ea2c03a731d922cfe291a6ed364574bca11
Patesaus77 <Emregulec70@gmail.com>
2025-03-05 14:26:28 +0100

    Added versioning to the application

--- webservice-api/routes/api.php
+++ webservice-api/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\SignsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TestController as TestV1Controller;
+use App\Http\Controllers\Api\V2\TestController as TestV2Controller;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +18,13 @@ Route::apiResource('signs',SignsController::class);
 Route::apiResource('categories',CategoriesController::class);
 //Route::apiResource('lessons',Lesson::class);
 
+Route::prefix('v1')->group(function () {
+    Route::get('/test', [TestV1Controller::class, 'index']);
+
+});
+
+//routes group version 2
+Route::prefix('v2')->group(function () {
+    Route::get('/test', [TestV2Controller::class, 'index']);
+
+});
