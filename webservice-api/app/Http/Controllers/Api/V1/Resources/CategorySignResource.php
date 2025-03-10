7994db8d8f6fee3f8085c9e7d3db5111fe21023b
Patesaus77 <Emregulec70@gmail.com>
2025-03-10 11:59:50 +0100

    controllers and Resources moved to v1 for versioning

--- webservice-api/app/Http/Controllers/Api/V1/Resources/CategorySignResource.php
+++ webservice-api/app/Http/Controllers/Api/V1/Resources/CategorySignResource.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace App\Http\Resources;
+namespace App\Http\Controllers\Api\V1\Resources;
 
-use Illuminate\Database\Eloquent\JsonEncodingException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use JsonException;
 
 class CategorySignResource extends JsonResource
 {
--- webservice-api/routes/api.php
+++ webservice-api/routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\CategoriesController;
-use App\Http\Controllers\SignsController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\CategoriesController;
+use App\Http\Controllers\Api\V1\SignsController;
+use App\Http\Controllers\Api\V1\LessonsController;
 use App\Http\Controllers\Api\V1\TestController as TestV1Controller;
 use App\Http\Controllers\Api\V2\TestController as TestV2Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,13 +16,18 @@ Route::get('/user', function (Request $request) {
 
 // the route starts with api/
 // To define a route u need to use apiResource see the example below
-Route::apiResource('signs',SignsController::class);
+//Route::apiResource('signs',SignsController::class);
+//
+//Route::apiResource('categories',CategoriesController::class);
 
-Route::apiResource('categories',CategoriesController::class);
-//Route::apiResource('lessons',Lesson::class);
 
 Route::prefix('v1')->group(function () {
     Route::get('/test', [TestV1Controller::class, 'index']);
+
+    Route::apiResource('signs',SignsController::class);
+    Route::apiResource('categories',CategoriesController::class);
+    Route::apiResource('lessons',LessonsController::class);
+
 
 });
 
