b5eb82651eec38acd33366a210900748abed140d
StefanMuijs <145674829+StefanMuijs@users.noreply.github.com>
2025-03-11 13:24:04 +0100

    Updated video path SignsController

--- app/Http/Controllers/Api/V1/SignsController.php
+++ app/Http/Controllers/Api/V1/SignsController.php
@@ -34,6 +34,7 @@ class SignsController extends Controller
             '*.video' => 'nullable|string',
         ]);
 
+
         $signs = [];
         foreach ($validatedData as $data) {
             $sign = new Sign();
@@ -58,9 +59,11 @@ class SignsController extends Controller
      */
     public function show(Sign $sign)
     {
-        //
+        $sign->video = $sign->video ? asset('storage/videos/' . $sign->video) : null;
+
         return new SignResource($sign);
     }
+
 
     /**
      * Update the specified resource in storage.
