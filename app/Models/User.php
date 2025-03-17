3e4136ba9db4e86f0fc6611c4eb7d200eff43e80
meesh <meeshoff2006@gmail.com>
2025-03-17 13:54:40 +0100

    Updated User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'sso_token',
+        'token',
         'api_token',
         'last_login_at',
     ];
