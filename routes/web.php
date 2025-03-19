391f027eca405cbbcfab65ad94f2484db7f21a60
Obada-Fa <ubadafatta@gmail.com>
2025-03-19 10:34:29 +0100

    een fix for the/developer route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\Api\V2\V2LoginController;
 use Illuminate\Support\Facades\Route;
 
 
-
+Route::get('/developer', function () {
+    return view('developer.index');
+});
 
 // ✅ Browser Login Route
 Route::get('/login', [V2LoginController::class, 'login']);
